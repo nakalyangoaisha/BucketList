b451af053bfb1455d3018302517e36b76c6632cd
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-07-12 17:34:21 +0300

    Modify app models

--- app/models.py
+++ app/models.py
@@ -9,22 +9,22 @@ class BucketList(object):
 
     def __init__(self):
 
-        self.items = {}
+        self.bucketlists = {}
 
     def add_bucketlist(self, title):
-        self.items.update({title: []})
+        self.bucketlists.update({title: []})
 
-    def edit_title(self, title):
-        if title in self.items.keys():
-            pass
+    def edit_title(self, title, new_title):
+        if title in self.bucketlists.keys():
+            self.bucketlists[new_title] = self.bucketlists.pop(title)
 
     def add_items(self, title, item):
-        if title in self.items.keys():
-            self.items.update({title: item})
+        if title in self.bucketlists.keys():
+            self.bucketlists.update({title: item})
 
-    def edit_items(self, item):
-        if item in self.items.values():
-            self.items.update({})
+    # def edit_items(self, item, new_item):
+    #         item_index = self.bucketlists.
+
 
 
 
