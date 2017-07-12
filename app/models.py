cdb0d62264d080d64e6397c6156f945e31816f0b
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-07-12 22:28:31 +0300

    Update app models

--- app/models.py
+++ app/models.py
@@ -11,21 +11,26 @@ class BucketList(object):
 
         self.bucketlists = {}
 
-    def add_bucketlist(self, title):
-        self.bucketlists.update({title: []})
-
-    def edit_title(self, title, new_title):
-        if title in self.bucketlists.keys():
-            self.bucketlists[new_title] = self.bucketlists.pop(title)
-
-    def add_items(self, title, item):
+    def additems(self, title, item):
         if title in self.bucketlists.keys():
             self.bucketlists.update({title: item})
+            return True
+        return False
+
+    def edittitle(self, title, new_title):
+        if title in self.bucketlists.keys():
+            self.bucketlists[new_title] = self.bucketlists.pop(title)
+            return True
+        return False
 
     # def edit_items(self, item, new_item):
     #         item_index = self.bucketlists.
 
-
+    def deletelist(self, title):
+        if title in self.bucketlists.keys():
+            del(self.bucketlists[title])
+            return True
+        return False
 
 
 
